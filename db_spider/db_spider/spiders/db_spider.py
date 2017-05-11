62d5878adbc343d6b2a9e7c21ab160b1580330d0
luoshao23 <luoshao23@gmail.com>
2017-05-11 13:49:25 +0800

    modified:   db_spider/db_spider/spiders/db_spider.py

--- db_spider/db_spider/spiders/db_spider.py
+++ db_spider/db_spider/spiders/db_spider.py
@@ -2,10 +2,18 @@ import scrapy
 
 class db_Spider(scrapy.Spider):
     name = "douban"
-    allowed_domains = ["movie.douban.com"]
-    start_urls = [ "https://movie.douban.com/subject/25937854/" ]
+	allowed_domains = ["movie.douban.com"]
+	start_urls = [ "https://movie.douban.com/subject/25937854/" ]
+	tmp = []
+
+
+    def start_request(self):
+    	urls = [ "https://movie.douban.com/subject/25937854/" ]
+    	for url in urls:
+    		yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         filename = response.url.split("/")[-2] + '.html'
         with open(filename, 'wb') as f:
         	f.write(response.body)
+        self.log('Saved file %s' % filename)
